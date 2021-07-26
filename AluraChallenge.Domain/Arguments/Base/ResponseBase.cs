7fb17ae776caaa2b6194c27852cde972277919e0
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2021-07-26 13:16:28 -0300

    Edit: Alterando pasta

---
+++ AluraChallenge.Domain/Arguments/Base/ResponseBase.cs
@@ -0,0 +1,14 @@
+﻿namespace AluraChallenge.Domain.Arguments
+{
+    public class ResponseBase
+    {
+        public ResponseBase(string message = "Operação realizada com sucesso")
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+
+        public string Id { get; set; }
+    }
+}
